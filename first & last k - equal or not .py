a352b8bf78ab3564db9402fb6d94c9795fb438f2
Rambalaji25 <92376545+Rambalaji25@users.noreply.github.com>
2021-11-18 19:55:53 +0530

    Create first & last k - equal or not .py

---
+++ first & last k - equal or not .py
@@ -0,0 +1,51 @@
+The program must accept a string S and an integer K as the input. The program must print YES if the first K characters are equal to the last K characters in S. Else the program must print NO as the output.
+
+Boundary Condition(s):
+1 <= K <= Length of S <= 100
+
+Input Format:
+The first line contains S.
+The second line contains K.
+
+Output Format:
+The first line contains YES or NO.
+
+Example Input/Output 1:
+Input:
+marksmar
+3
+
+Output:
+YES
+
+Explanation:
+The first 3 characters in marksmar are m, a and r.
+The last 3 characters in marksmar are m, a and r.
+Here the first 3 characters are equal to the last 3 characters in marksmar.
+Hence the output is YES
+
+Example Input/Output 2:
+Input:
+ring
+4
+
+Output:
+YES
+
+Example Input/Output 3:
+Input:
+skillrackskill
+6
+
+Output:
+NO
+
+
+s=input().strip()
+n=int(input())
+d1=s[:n]
+d2=s[-n:]
+if d1==d2:
+    print("YES")
+else:
+    print("NO")
